96d13e4d8e995aa25246f93ed34ac713d47d00ee
505440 <chris.greene@seagate.com>
2014-12-10 16:23:51 -0700

    no message

--- deployvCACwithPowerCLI.ps1
+++ deployvCACwithPowerCLI.ps1
@@ -21,7 +21,7 @@ $ovfInfo = @{
     ipAddress  = '192.168.3.88';
   };
   VMware_vRealize_Appliance = @{
-    path       = 'z:\VMware-vCAC-Appliance-6.2.0.0-2330392_OVF10.ova';
+    path       = 'z:\vra\VMware-vCAC-Appliance-6.2.0.0-2330392_OVF10.ova';
     hostname   = 'vra62a.vmware.local';
     ipAddress  = '192.168.3.89';
   };
